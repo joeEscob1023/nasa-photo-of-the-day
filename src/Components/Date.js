b36e83bb2589973a668d0699bcca909d7078ded4
Joe Escobedo <joe_escob1023@live.com>
2021-06-09 16:18:10 -0700

    trying to create a calender where the user can pick the date so a new image comes up

--- src/Components/Date.js
+++ src/Components/Date.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import Styled from "styled-components";
+
+const StyledDate = Styled.div`
+margin: 0 auto;
+width: 20%;
+
+`;
+
+//const (locale, date) => formatDate(date, "dd MMM YYYY")
 
 const Date = (props) => {
   const { date } = props.dateInfo.state;
+
   console.log(date);
+  const [value, onChange] = useState(date);
   return (
-    <div>
-      <p>Date: {date}</p>
-    </div>
+    // <div>
+    //   <p>Date: {date}</p>
+    // </div>
+    <StyledDate>
+      <Calendar onChange={onChange} value={value} />
+    </StyledDate>
   );
 };
 
